76b98afb81329be71c354338f0cce4d48a1566cf
elichild01 <elichild01@gmail.com>
2024-05-23 22:33:45 -0600

    feat(chess) implement isInCheckmate. Most GameStatusTests passing.

--- shared/src/main/java/chess/ChessGame.java
+++ shared/src/main/java/chess/ChessGame.java
@@ -157,7 +157,19 @@ public class ChessGame {
      * @return True if the specified team is in checkmate
      */
     public boolean isInCheckmate(TeamColor teamColor) {
-        throw new RuntimeException("Not implemented");
+        if (!isInCheck(teamColor)) { return false; }
+        if (teamTurn != teamColor) { return false; }
+        for (int row = 1; row <= 8; row++) {
+            for (int col = 1; col <= 8; col++) {
+                ChessPosition currPosition = new ChessPosition(row, col);
+                ChessPiece piece = board.getPiece(currPosition);
+                if (piece != null && piece.getTeamColor()==teamColor) {
+                    int numPossibleMoves = validMoves(currPosition).size();
+                    if (numPossibleMoves > 0) { return false; }
+                }
+            }
+        }
+        return true;
     }
 
     /**

a6f931a2b07098d66ab0c1fc11769fe85102c52a
elichild01 <elichild01@gmail.com>
2024-07-09 09:06:27 -0600

    feat(phase3) stubs of MemoryGameDAO, doesn't yet pass tests

---
+++ server/src/main/java/dataaccess/MemoryGameDAO.java
@@ -0,0 +1,34 @@
+package dataaccess;
+
+import chess.ChessGame;
+import model.GameData;
+
+import java.sql.Array;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
+
+public class MemoryGameDAO implements GameDAO {
+    private int nextId = 1;
+    final private HashMap<Integer, GameData> games = new HashMap<>();
+
+    public GameData createGame(String gameName) throws DataAccessException {
+        String whiteUsername = "";
+        String blackUsername = "";
+        int gameId = nextId++;
+        ChessGame game = new ChessGame();
+
+        GameData gameData = new GameData(gameId, whiteUsername, blackUsername, gameName, game);
+        games.put(gameId, gameData);
+        return gameData;
+    }
+
+    public void deleteAllGames() {
+        games.clear();
+    }
+
+    public Collection<GameData> listAllGames() {
+        Collection<GameData> allGames = new ArrayList<>(games.values());
+        return allGames;
+    }
+}

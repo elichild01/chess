656a7b40ac92c2ad199183b9d24424c947fbd258
elichild01 <elichild01@gmail.com>
2024-07-09 09:05:32 -0600

    feat(phase3) wrote GameService

---
+++ server/src/main/java/service/GameService.java
@@ -0,0 +1,23 @@
+package service;
+
+import dataaccess.GameDAO;
+import model.GameData;
+
+import java.util.Collection;
+
+public class GameService {
+    private final GameDAO gameDb;
+
+    public Collection<GameData> listAllGames() {
+
+    }
+
+    public GameData createGame(String gameName) {
+
+    }
+
+    public void deleteAllGames() {
+
+    }
+
+}

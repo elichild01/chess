db9392faf2bc4f67f0aa2599567eaf40de9c2c1c
elichild01 <elichild01@gmail.com>
2024-07-19 09:42:00 -0600

    feat(phase4) UserDataAccessTest extended to refer to MemoryUserDataAccess, all user data access tests written.

---
+++ server/src/test/java/dataaccess/UserDataAccessTest.java
@@ -0,0 +1,89 @@
+package dataaccess;
+
+import model.UserData;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+import org.mindrot.jbcrypt.BCrypt;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserDataAccessTest {
+
+    private static UserData existingUser;
+    private static UserData newUser;
+
+    @BeforeAll
+    public static void init() {
+        String hashedExistingPassword = BCrypt.hashpw("existingPassword", BCrypt.gensalt());
+        String hashedNewPassword = BCrypt.hashpw("newPassword", BCrypt.gensalt());
+        existingUser = new UserData("existingUsername", hashedExistingPassword, "existingEmail");
+        newUser = new UserData("newUsername", hashedNewPassword, "newEmail");
+    }
+
+    // getUser
+    @ParameterizedTest
+    @ValueSource(classes={MySQLUserDataAccess.class, MemoryUserDataAccess.class})
+    void getUserReturnsCorrectUsername(Class<? extends UserDataAccess> databaseClass) throws Exception {
+        var database = databaseClass.getDeclaredConstructor().newInstance();
+        database.addUser(existingUser);
+        UserData retrievedUser = database.getUser(existingUser.username());
+        assertEquals(retrievedUser.username(), existingUser.username());
+    }
+    @ParameterizedTest
+    @ValueSource(classes={MySQLUserDataAccess.class, MemoryUserDataAccess.class})
+    void getUserNonexistentUserReturnsNull(Class<? extends UserDataAccess> databaseClass) throws Exception {
+        var database = databaseClass.getDeclaredConstructor().newInstance();
+        database.addUser(existingUser);
+        UserData user = database.getUser("not-a-username");
+        assertNull(user);
+    }
+
+    // addUser
+    @ParameterizedTest
+    @ValueSource(classes={MySQLUserDataAccess.class, MemoryUserDataAccess.class})
+    void addUser(Class<? extends UserDataAccess> databaseClass) throws Exception {
+        var database = databaseClass.getDeclaredConstructor().newInstance();
+        assertDoesNotThrow(() -> database.addUser(newUser));
+    }
+    @ParameterizedTest
+    @ValueSource(classes={MySQLUserDataAccess.class, MemoryUserDataAccess.class})
+    void addUserDuplicateUsernameThrowsException(Class<? extends UserDataAccess> databaseClass) throws Exception {
+        var database = databaseClass.getDeclaredConstructor().newInstance();
+        database.addUser(existingUser);
+        try {
+            database.addUser(existingUser);
+        } catch (DataAccessException ex) {
+            assertInstanceOf(DataAccessException.class, ex);
+            assertEquals(ex.getMessage(), "already taken");
+        }
+    }
+
+    // deleteAllUsers
+    @ParameterizedTest
+    @ValueSource(classes={MySQLUserDataAccess.class, MemoryUserDataAccess.class})
+    void deleteAllUsers(Class<? extends UserDataAccess> databaseClass) throws Exception {
+        var database = databaseClass.getDeclaredConstructor().newInstance();
+        database.addUser(existingUser);
+        assertDoesNotThrow(database::deleteAllUsers);
+        assertEquals(0, database.getNumUsers());
+    }
+
+    // getNumUsers
+    @ParameterizedTest
+    @ValueSource(classes={MySQLUserDataAccess.class, MemoryUserDataAccess.class})
+    void getNumUsers(Class<? extends UserDataAccess> databaseClass) throws Exception {
+        var database = databaseClass.getDeclaredConstructor().newInstance();
+        assertEquals(0, database.getNumUsers());
+        database.addUser(existingUser);
+        assertEquals(1, database.getNumUsers());
+    }
+    @Test
+    void getNumUsersNullDatabaseThrowsNullException() {
+        MySQLUserDataAccess sqlUserDataAccess = null;
+        MemoryUserDataAccess memUserDataAccess = null;
+        assertThrows(NullPointerException.class, () -> sqlUserDataAccess.getNumUsers());
+        assertThrows(NullPointerException.class, () -> memUserDataAccess.getNumUsers());
+    }
+}

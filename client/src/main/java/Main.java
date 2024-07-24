a198fb717a4b0b0e7663a40296b2551455dd7d86
elichild01 <elichild01@gmail.com>
2024-07-23 22:29:14 -0600

    feat(phase5) initial skeleton of pre-login loop. Code compiles with Maven, although dependencies not yet resolved.

--- client/src/main/java/Main.java
+++ client/src/main/java/Main.java
@@ -1,8 +1,37 @@
 import chess.*;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        var piece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
-        System.out.println("♕ 240 Chess Client: " + piece);
+//        var piece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
+//        System.out.println("♕ 240 Chess Client: " + piece);
+
+        AppState state = AppState.PRELOGIN;
+
+        System.out.println("Welcome to Eli's 240 Chess. Type 'help' to get started.");
+        Scanner scanner = new Scanner(System.in);
+        String userSelection = scanner.nextLine();
+
+        while (!getUserOptions(state).contains(userSelection.getFirstWord())) {
+            System.out.println("Option not recognized. Type 'help' to get started.");
+        }
+
+
     }
+
+    private enum AppState {
+        PRELOGIN,
+        POSTLOGIN
+    }
+
+    private static String[] getUserOptions(AppState state) {
+        return switch (state) {
+            case PRELOGIN -> new String[]{"help", "quit", "login", "register"};
+            case POSTLOGIN -> new String[]{"help", "logout", "create", "list", "play", "observe"};
+        };
+    }
+
 }

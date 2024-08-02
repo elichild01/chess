563115eac51b1b9c384ad6b2f7382417a3f888d6
elichild01 <elichild01@gmail.com>
2024-08-01 21:16:43 -0600

    feat(phase6) make initial near-record classes for all messages and commands

---
+++ shared/src/main/java/websocket/messages/LoadGameMessage.java
@@ -0,0 +1,15 @@
+package websocket.messages;
+
+import model.GameData;
+
+public class LoadGameMessage extends ServerMessage {
+    private final GameData game;
+    public LoadGameMessage(ServerMessageType type, GameData game) {
+        super(type);
+        this.game = game;
+    }
+
+    public GameData getGame() {
+        return this.game;
+    }
+}
